65c2e394c6b8855e814ad81f9954cb6110e1eff3
rohan37-pro <rohan2002maji@gmail.com>
2024-09-25 17:06:18 +0530

    dounutLand randering issue is resolved

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import doughnutLand from './doughnutLand.jsx'
+import DoughnutLand from './doughnutLand.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <doughnutLand />
+    <DoughnutLand/>
   </StrictMode>,
 )
