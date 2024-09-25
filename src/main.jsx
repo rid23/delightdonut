656ae7596e35c28ad09164f5eefd9c07b31b08ef
Sushobhan Patra <patrasushobhan2003@gmail.com>
2024-09-26 01:30:15 +0530

    merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -9,15 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import DoughnutLand from './doughnutLand.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <DoughnutLand/>
-  </StrictMode>,
-)
->>>>>>> 65c2e394c6b8855e814ad81f9954cb6110e1eff3
